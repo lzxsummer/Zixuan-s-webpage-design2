2a984c5b5f5b0e101f02eb103aaacec5d1f7bb6b
lin zixuan <lzxsummer.home@gmail.com>
2018-10-13 18:16:23 +0200

    Add files via upload

---
+++ js/course15.js
@@ -0,0 +1,8 @@
+<script>
+  function myFunction(){
+  	var x = document.getElementsByClassName("block1");
+  	for (var i = x.length - 1; i >= 0; i--) {
+  		x[i].style.display = "none";
+  	}
+  }
+</script>
